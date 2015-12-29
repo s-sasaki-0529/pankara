523e88aa18eb84dab869ca7407c993d375954543
Sa2Knight <shingo.sasaki.0529@gmail.com>
2015-12-29 06:58:39 +0000

    ref #87 コメントを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,14 @@ class User
 
 	attr_reader :params
 
+	# initialize(id:) - idを指定してインスタンスを生成
+	#---------------------------------------------------------------------
 	def initialize(id:)
 		@params = DB.sql_row("SELECT id , username , screenname FROM user WHERE id = ?" , [id])
 	end
 
+	# initialize(username:) - usernameを指定してインスタンスを生成
+	#---------------------------------------------------------------------
 	def initialize(username:)
 		@params = DB.sql_row("SELECT id , username , screenname FROM user WHERE username = ?" , [username])
 	end
