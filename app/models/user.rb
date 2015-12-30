f6c6016d200d29a517f88b8aebe0f323be9779ef
Sa2Knight <shingo.sasaki.0529@gmail.com>
2015-12-30 23:58:35 +0900

    Rubyはコンストラクタのオーバーロードできないようなので修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,6 @@ class User
 
 	attr_reader :params
 
-	# initialize(id:) - idを指定してインスタンスを生成
-	#---------------------------------------------------------------------
-	def initialize(id:)
-		@params = DB.sql_row("SELECT id , username , screenname FROM user WHERE id = ?" , [id])
-	end
-
 	# initialize(username:) - usernameを指定してインスタンスを生成
 	#---------------------------------------------------------------------
 	def initialize(username:)
