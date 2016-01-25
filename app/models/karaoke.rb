d2b957e5fc5470c6c96be283ee1f17f42b18d73f
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-01-25 20:14:05 +0900

    refs #207 カラオケ詳細画面内のリンクを修正

--- app/models/karaoke.rb
+++ app/models/karaoke.rb
@@ -80,7 +80,7 @@ class Karaoke < Base
 			song = Song.new(history['song'])
 			history['song_id'] = song.params['id']
 			history['song_name'] = song.params['name']
-			history['artist_id'] = song.params['artist'['name']]
+			history['artist_id'] = song.params['artist']
 			history['artist_name'] = song.params['artist_name']
 			history['userinfo'] = users_info.find { |user| user['attendance'] == history['attendance'] }
 			history['scoretype_name'] = ScoreType.id_to_name(history['score_type'])
