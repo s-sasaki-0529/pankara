50adfcf2a268dc48e1e26bd915a6c00f829ec5b8
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-01-26 00:07:38 +0900

    (no ticket) DBの使い方を修正

--- app/models/history.rb
+++ app/models/history.rb
@@ -13,16 +13,18 @@ class History < Base
 	# song_ranking - クラスメソッド: 楽曲の歌唱数ランキングを取得
 	#---------------------------------------------------------------------
 	def self.song_ranking(limit)
-		db = DB.new
-		db.select({
-			'song.id' => 'song_id' ,
-			'song.name' => 'song_name' ,
-			'song.artist' => 'artist_id' ,
-			'song.url' => 'song_url' ,
-			'artist.name' => 'artist_name' ,
-			'count(*)' => 'count'
-		})
-		db.from('history')
+		db = DB.new(
+			:SELECT => {
+				'song.id' => 'song_id' ,
+				'song.name' => 'song_name' ,
+				'song.artist' => 'artist_id' ,
+				'song.url' => 'song_url' ,
+				'artist.name' => 'artist_name' ,
+				'count(*)' => 'count'
+			} ,
+			:FROM => 'history' ,
+			:OPTION => "GROUP BY history.song ORDER BY count DESC LIMIT #{limit}"
+		)
 		db.join(
 			['history' , 'song'] ,
 			['song' , 'artist']
