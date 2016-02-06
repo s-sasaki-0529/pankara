1b25db2fff4d51577d1ada3d384711040cb2be6e
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-02-06 17:40:58 +0900

    refs #243

--- app/controllers/ranking_route.rb
+++ app/controllers/ranking_route.rb
@@ -9,4 +9,11 @@ class RankingRoute < March
 		erb :song_ranking
 	end
 
+	# get '/ranking/artist' - 歌手別の歌唱回数ランキングを表示
+	#---------------------------------------------------------------------
+	get '/ranking/artist' do
+		@artists = Ranking.artist_sang_count
+		erb :artist_ranking
+	end
+
 end
