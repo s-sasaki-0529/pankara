02e8f7c1a63fb3f70ef9edbb1d99ebd644bd0a47
unagipai222 <unagipai222@gmail.com>
2016-02-12 22:25:37 +0900

    refs #279 zenra.jsを整理した

--- app/public/script/zenra.js
+++ app/public/script/zenra.js
@@ -2,56 +2,16 @@
 zenra = {};
 
 /*
-resetHistory - 歌唱履歴入力欄をリセットする
+post - 情報を非同期で送信する
 */
-zenra.resetHistory = function() {
-	$('#song').val('');
-	$('#artist').val('');
-	$('#score').val('');
-};
-
-/*
-postHistory - 歌唱履歴情報を送信する
-*/
-zenra.postHistory = function(button) {
+zenra.post = function(url , data , funcs) {
+	funcs = funcs || {};
 	$.ajax({
 		type: "POST" ,
-		url: '/history/input' ,
-		data: {
-			song: $('#song').val() ,
-			artist: $('#artist').val() ,
-			score: $('#score').val() ,
-			songkey: $('#seekbar').slider('value') ,
-			score_type: $('#score_type').val() ,
-		} ,
-		complete: function() {
-			if (button == 'regist') {
-				location.href = '/history/regist';
-			}
-		} ,
+		url: url ,
+		data: data ,
+		complete: funcs.complete
 	});
-
-	zenra.resetHistory();
-};
-
-/*
-postHistory - カラオケ情報を送信する
-*/
-zenra.postKaraoke = function() {
-	$.ajax({
-		type: "POST",
-		url: '/karaoke/input',
-		data: {
-			name: $('#name').val(),
-			datetime: $('#datetime').val(),
-			plan: $('#plan').val(),
-			store: $('#store').val(),
-			branch: $('#branch').val(),
-			product: $('#product').val(),
-		},
-	});
-	
-	zenra.transitionInDialog('/_local/dialog' , 'input_history');
 };
 
 /*
@@ -71,7 +31,6 @@ zenra.showDialog = function(title, url , id , width) {
 				$(event.target).remove();
 			} ,
 		});
-		init_ui();
 	});
 };
 
@@ -83,9 +42,28 @@ zenra.transitionInDialog = function(url , id) {
 
 	jQuery.removeData(div);
 	div.load(url + " #" + id , function(date , status) {
-		init_ui();
+		zenra.createSeekbar();
 	});
 };
+	
+/*
+createSeekbar - シークバーを作成する
+*/
+zenra.createSeekbar = function() {
+	$('#seekbar').slider({
+		value: 0 ,
+		max: 6 ,
+		min: -6 ,
+		step: 1 ,
+
+		create: function() {
+			$("#slidervalue").html('キー: ' + $('#seekbar').slider('value'));
+		} ,
+		change: function() {
+			$('#slidervalue').html('キー: ' + $('#seekbar').slider('value'));
+		} ,
+	});
+}
 
 /*
 	bathtowelオブジェクト -バスタオルの制御全般-
@@ -117,3 +95,61 @@ bathtowel = {
 	} ,
 
 };
+
+/*
+	registerクラス -カラオケ入力制御全般-
+*/
+var register = (function() {
+	
+	/*[Method] 歌唱履歴入力欄をリセットする*/
+	function resetHistory() {
+		$('#song').val('');
+		$('#artist').val('');
+		$('#score').val('');
+		$('input[name=song]').focus();
+	}
+
+	return {
+		/*[Method] 履歴入力用ダイアログを作成する*/
+		createDialog : function() {
+			zenra.showDialog('カラオケ入力' , '/_local/dialog' , 'input_karaoke' , 600);
+		} ,
+
+		/*[Method] カラオケ情報入力終了後の処理*/
+		execInputKaraoke : function() {
+			data = {
+				name: $('#name').val() ,
+				datetime: $('#datetime').val() ,
+				plan: $('#plan').val() ,
+				store: $('#store').val() ,
+				branch: $('#branch').val() ,
+				product: $('#product').val() ,
+			};
+	
+			zenra.post('/karaoke/input' , data);
+			zenra.transitionInDialog('/_local/dialog' , 'input_history');
+		} ,
+	
+		/*[Method] 歌唱履歴情報入力終了後の処理*/
+		execInputHistory : function(button) {
+			data = {
+				song: $('#song').val() ,
+				artist: $('#artist').val() ,
+				score: $('#score').val() ,
+				songkey: $('#seekbar').slider('value') ,
+				score_type: $('#score_type').val() ,
+			};
+	
+			funcs = {};
+			if (button == 'regist') {
+				funcs.complete = function() {
+					location.href = '/history/regist';
+				};
+			}
+	
+			zenra.post('/history/input' , data , funcs);
+			resetHistory();
+		} ,
+	}
+	
+})();
