6087531941a246114dd4f5edc35c5bc3ddccefc3
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-02-26 22:03:41 +0900

    refs #338 AttendanceクラスにDELETEメソッドを追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -10,4 +10,30 @@ class Attendance < Base
     @params = DB.new.get('attendance' , id)
   end
 
+  # delete - レコードを削除
+  #....................................................................
+  def delete
+
+    # 参照しているhistoryから削除する
+    histories = DB.new(
+      :SELECT => {'history.id' => 'id'} ,
+      :FROM => 'history' ,
+      :JOIN => ['history' , 'attendance'] ,
+      :WHERE => 'attendance.id = ?' ,
+      :SET => @params['id'] ,
+    ).execute_columns
+    qlist = Util.make_questions(histories.length)
+    DB.new(
+      :DELETE => 1 ,
+      :FROM => 'history' ,
+      :WHERE => "id in ( #{qlist} )" ,
+      :SET => histories
+    ).execute
+
+    #attendanceレコードを削除
+    DB.new(:DELETE => 1 , :FROM => 'attendance' , :WHERE => 'id = ?' , :SET => @params['id']).execute
+    @params = nil
+
+  end
+
 end
