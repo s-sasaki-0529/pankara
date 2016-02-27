8c7ae56b7f21d5f50c39c662db40c8831dd6eeb3
unagipai222 <unagipai222@gmail.com>
2016-02-27 10:52:42 +0900

    /local/rpc/karaokelist 微修正(no ticket)

--- app/controllers/local_route.rb
+++ app/controllers/local_route.rb
@@ -35,7 +35,7 @@ class LocalRoute < March
   # post '/local/rpc/karaokelist/?' - カラオケの一覧もしくは指定したカラオケを戻す
   #---------------------------------------------------------------------
   post '/local/rpc/karaokelist/?' do
-    params[:id].nil? ? Karaoke.list_all : Util.to_json(Karaoke.new(params[:id]).params)
+    params[:id].nil? ? Util.to_json(Karaoke.list_all) : Util.to_json(Karaoke.new(params[:id]).params)
   end
   
   # post '/local/rpc/karaoke/delete/:id' - カラオケを削除する
