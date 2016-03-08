7a49e2227045f55fca385859bd7c3d5e52c85752
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-03-08 22:11:03 +0900

    refs #375

--- app/controllers/song_route.rb
+++ app/controllers/song_route.rb
@@ -6,13 +6,14 @@ class SongRoute < March
   #---------------------------------------------------------------------
   get '/song/:id' do
     score_type = 1 #現在は仮で固定
+    user = @current_user.params['id']
     @song         = Song.new(params[:id])
     @sangcount    = @song.sangcount()
     @score        = @song.tally_score(score_type)
-    @history      = @song.history_list(10)
-    @my_sangcount = @song.sangcount(@current_user.params['id'])
-    @my_score     = @song.tally_score(score_type , @current_user.params['id'])
-    @my_history   = @song.history_list(10 , @current_user.params['id'])
+    @history      = @song.history_list({:history => 10 , :other_user => user})
+    @my_sangcount = @song.sangcount(user)
+    @my_score     = @song.tally_score(score_type , user)
+    @my_history   = @song.history_list({:history => 10 , :target_user => user})
     erb :song_detail
   end
 
