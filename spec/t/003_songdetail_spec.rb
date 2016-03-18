6f624bb5b36d8d0a5d47bffdb2092aef629d312a
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-03-19 00:17:24 +0900

    refs #421 既存テストを実データを用いたものに書き直し(一部)

--- spec/t/003_songdetail_spec.rb
+++ spec/t/003_songdetail_spec.rb
@@ -3,28 +3,8 @@ include Rbase
 
 # テスト用データベース構築
 init = proc do
-  `zenra init`
-  User.create('sa2knight' , 'sa2knight' , 'ないと')
-  User.create('tomotin' , 'tomotin' , 'ともちん')
-  score_type = {'brand' => 'JOYSOUND' , 'name' => '全国採点'}
-
-  register1 = Register.new(User.new('sa2knight'))
-  karaoke_id = register1.create_karaoke(
-    '2016-01-05 11:00:00' , '楽曲詳細ページテスト用カラオケ' , 3 ,
-    {'name' => 'カラオケ館' , 'branch' => '亀戸店'} ,
-    {'brand' => 'JOYSOUND' , 'product' => 'MAX'} ,
-  )
-  register1.attend_karaoke(1000 , '楽曲詳細ページテスト用attend1')
-  register2 = Register.new(User.new('tomotin'))
-  register2.karaoke = karaoke_id
-  register2.attend_karaoke(1200 , '楽曲詳細ページテスト用attend2')
-  
-  score1 = [21.6 , 47.3 , 57.1 , 68.3 , 76.2 , 80.9 , 90 , 93.8 , 98.2 , 99.90]
-  score2 = [2.4 , 38.5 , 70.2 , 79 , 82.5 , 90.4 , 94.2 , 100 , 100 , 100]
-  0.upto(9) do |i|
-    register1.create_history('ゼロ' , 'BUMP OF CHICKEN' , 0 , score_type , score1[i])
-    register2.create_history('ゼロ' , 'BUMP OF CHICKEN' , 0 , score_type , score2[i])
-  end
+  `zenra init -d 2016_03_14_21_21`
+  `zenra mysql -e 'update history set song = 1 , score_type = 1 where score > 90'`
 end
 
 # テスト実行
@@ -33,38 +13,41 @@ describe '楽曲詳細ページ' do
   it '得点の集計が正常に表示されるか' do
     login 'sa2knight'
     visit '/history'
-    examine_songlink('ゼロ' , 'BUMP OF CHICKEN')
-    examine_text('my_sangcount' , 'あなた: 10')
-    examine_text('my_maxscore' , '最高: 99.90')
-    examine_text('my_minscore' , '最低: 21.60')
-    examine_text('my_avgscore' , '平均: 73.33')
-    examine_text('sangcount' , 'みんな: 20')
-    examine_text('maxscore' , '最高: 100.00')
-    examine_text('minscore' , '最低: 2.40')
-    examine_text('avgscore' , '平均: 74.52')
-    login 'tomotin'
+    examine_songlink('オンリーロンリーグローリー' , 'BUMP OF CHICKEN')
+    examine_text('my_sangcount' , 'あなた: 14')
+    examine_text('my_maxscore' , '最高: 97.00')
+    examine_text('my_minscore' , '最低: 88.39')
+    examine_text('my_avgscore' , '平均: 92.65')
+    examine_text('sangcount' , 'みんな: 5')
+    examine_text('maxscore' , '最高: 97.00')
+    examine_text('minscore' , '最低: 93.00')
+    examine_text('avgscore' , '平均: 94.60') 
+    login 'hetare'
     visit '/history'
-    examine_songlink('ゼロ' , 'BUMP OF CHICKEN')
-    examine_text('my_sangcount' , 'あなた: 10')
-    examine_text('my_maxscore' , '最高: 100.00')
-    examine_text('my_minscore' , '最低: 2.40')
-    examine_text('my_avgscore' , '平均: 75.72')
+    examine_songlink('オンリーロンリーグローリー' , 'BUMP OF CHICKEN')
+    examine_text('my_sangcount' , 'あなた: 2')
+    examine_text('my_maxscore' , '最高: 97.00')
+    examine_text('my_minscore' , '最低: 95.00')
+    examine_text('my_avgscore' , '平均: 96.00')
+    examine_text('sangcount' , 'みんな: 17')
+    examine_text('maxscore' , '最高: 97.00')
+    examine_text('minscore' , '最低: 88.39')
+    examine_text('avgscore' , '平均: 92.84')
   end
   it 'Youtubeがインラインで表示されているか' do
     login 'sa2knight'
     visit 'history'
-    examine_songlink('ゼロ' , 'BUMP OF CHICKEN')
-    db = DB.new(:SELECT => 'url' , :FROM => 'song' , :WHERE => 'name = ?' , :SET => 'ゼロ')
+    examine_songlink('オンリーロンリーグローリー' , 'BUMP OF CHICKEN')
+    db = DB.new(:SELECT => 'url' , :FROM => 'song' , :WHERE => 'name = ?' , :SET => 'オンリーロンリーグローリー')
     tube = db.execute_column
     expect(youtube_links[0].slice(/\w+$/)).to eq tube.slice(/\w+$/)
   end
-
   it 'リンクが正常に登録されているか' do
     login 'sa2knight'
     visit 'history'
-    examine_songlink('ゼロ' , 'BUMP OF CHICKEN')
+    examine_songlink('オンリーロンリーグローリー' , 'BUMP OF CHICKEN')
     song_url = page.current_path
-    examine_karaokelink('楽曲詳細ページテスト用カラオケ' , song_url)
+    examine_karaokelink('祝本番環境リリースカラオケ' , song_url)
     examine_userlink('ないと' , song_url)
   end
 
--- spec/t/004_artistdetail_spec.rb
+++ spec/t/004_artistdetail_spec.rb
@@ -3,52 +3,28 @@ include Rbase
 
 # テスト用データベース構築
 init = proc do
-  `zenra init`
-  User.create('sa2knight' , 'sa2knight' , 'ないと')
-  User.create('tomotin' , 'tomotin' , 'ともちん')
-
-  register = Register.new(User.new('sa2knight'))
-  register.with_url = false
-  karaoke_id = register.create_karaoke(
-    '2016-01-04 16:00:00' , '歌手詳細ページテスト用カラオケ' , 3 ,
-    {'name' => 'カラオケ館' , 'branch' => '亀戸店'} ,
-    {'brand' => 'JOYSOUND' , 'product' => 'MAX'} ,
-  )
-  register.attend_karaoke(1000 , '歌手詳細ページテスト用attend1')
-  5.times {|i| register.create_history('カルマ' , 'BUMP OF CHICKEN') }
-  4.times {|i| register.create_history('銀河鉄道' , 'BUMP OF CHICKEN')}
-  3.times {|i| register.create_history('stage of the ground' , 'BUMP OF CHICKEN')}
-
-  register = Register.new(User.new('tomotin'))
-  register.with_url = false
-  register.karaoke = karaoke_id
-  register.attend_karaoke(1200 , '歌手詳細ページテスト用attend2')
-  3.times {|i| register.create_history('カルマ' , 'BUMP OF CHICKEN') }
-  2.times {|i| register.create_history('銀河鉄道' , 'BUMP OF CHICKEN')}
-  1.times {|i| register.create_history('stage of the ground' , 'BUMP OF CHICKEN')}
+  `zenra init -d 2016_03_14_21_21`
+  `zenra mysql -e "update song set artist = 1 where id > 100"`
+  `zenra mysql -e "update song set url = NULL where id = 115"`
 end
-
-# 定数定義
-url = '/history'
 
 # テスト実行
 describe '歌手詳細ページ' do
   before(:all,&init)
-  it '歌手の楽曲一覧が正常に表示されるか' do
+  before do
     login 'sa2knight'
-    visit url
+    visit '/history'
+  end
+  it '歌手の楽曲一覧が正常に表示されるか' do
     examine_artistlink 'BUMP OF CHICKEN'
     tables = table_to_hash('artistdetail_table')
-    expect(tables.length).to eq 3
-    expect(tables[0]['tostring']).to eq '未登録,カルマ,5,8'
-    expect(tables[1]['tostring']).to eq '未登録,銀河鉄道,4,6'
-    expect(tables[2]['tostring']).to eq '未登録,stage of the ground,3,4'
+    expect(tables.length).to eq 174
+    expect(tables[0]['tostring']).to eq ',オンリーロンリーグローリー,2,0'
+    expect(tables[18]['tostring']).to eq '未登録,たったひとつの日々,0,2'
   end
   it 'リンクが正常に登録されているか' do
-    songs = ['カルマ' , '銀河鉄道' , 'stage of the ground']
-    login 'sa2knight'
+    songs = ['走れ' , '君に届け' , 'ロミオとシンデレラ']
     songs.each do |song|
-      visit url
       examine_artistlink 'BUMP OF CHICKEN'
       examine_songlink(song , 'BUMP OF CHICKEN')
     end
