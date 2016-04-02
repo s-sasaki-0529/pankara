c7fa2cbac66f2635148a7d3c752b006c4d53e9e4
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-04-02 14:07:24 +0900

    refs #430 完了。画面実装はしてない

--- app/controllers/ranking_route.rb
+++ app/controllers/ranking_route.rb
@@ -2,6 +2,13 @@ require_relative './march'
 
 class RankingRoute < March
 
+  # GETでshowmineが指定されている時、ログイン中ユーザを戻す
+  #--------------------------------------------------------------------
+  def target_user
+    @target_user = params[:showmine] && @current_user ? @current_user : nil
+    return @target_user
+  end
+
   # get '/ranking/score/?' - 得点ランキングを表示
   # get '/ranking/score/:score_type - 指定した採点モードの得点ランキングを表示
   #--------------------------------------------------------------------
@@ -10,7 +17,8 @@ class RankingRoute < March
   end
   get '/ranking/score/:score_type' do
     @current_score_type = params[:score_type]
-    @scores = Ranking.score({:score_type => @current_score_type})
+    param = {:score_type => @current_score_type , :user => target_user}
+    @scores = Ranking.score(param)
     @score_type = ScoreType.id_to_name(@current_score_type , true)
     @score_type_list = ScoreType.List
     erb :score_ranking
@@ -19,14 +27,14 @@ class RankingRoute < March
   # get '/ranking/song' - 楽曲の歌唱回数ランキングを表示
   #---------------------------------------------------------------------
   get '/ranking/song' do
-    @songs = Ranking.sang_count()
+    @songs = Ranking.sang_count({:user => target_user})
     erb :song_ranking
   end
 
   # get '/ranking/artist' - 歌手別の歌唱回数ランキングを表示
   #---------------------------------------------------------------------
   get '/ranking/artist' do
-    @artists = Ranking.artist_sang_count()
+    @artists = Ranking.artist_sang_count({:user => target_user})
     erb :artist_ranking
   end
 
