56a55f5c3e843222f462e37097d2b98222b1ef45
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-05-01 17:21:23 +0900

    refs #487 unicornの設定見直し

--- unicorn.rb
+++ unicorn.rb
@@ -2,8 +2,8 @@
 
 @path = `echo $ZENRA`
 @path.chomp!
-worker_processes 1
+worker_processes 2
 working_directory @path
-timeout 300
+timeout 30
 stderr_path "#{@path}/logs/stderr.log"
 stdout_path "#{@path}/logs/stdout.log"
