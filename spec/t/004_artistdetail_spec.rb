06a961decda58b33017f274e6252fc28d504b805
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-05-06 00:10:40 +0900

    refs #504 アーティスト詳細画面から、楽曲へのリンクが無いバグを修正

--- spec/t/004_artistdetail_spec.rb
+++ spec/t/004_artistdetail_spec.rb
@@ -1,8 +1,6 @@
 require_relative '../rbase'
 include Rbase
 
-url = '/history'
-
 # テスト用データベース構築
 init = proc do
   `zenra init -d 2016_03_14_21_21`
@@ -15,7 +13,7 @@ describe '歌手詳細ページ' do
   before(:all,&init)
   before do
     login 'sa2knight'
-    visit url
+    visit '/history'
   end
   it '歌手の楽曲一覧が正常に表示されるか' do
     examine_artistlink 'BUMP OF CHICKEN'
@@ -25,9 +23,11 @@ describe '歌手詳細ページ' do
     expect(tables[18]['tostring']).to eq '未登録,たったひとつの日々,0,2'
   end
   it 'リンクが正常に登録されているか' do
+    visit '/history'
+    examine_artistlink('BUMP OF CHICKEN')
+    url = page.current_url
     songs = ['走れ' , '君に届け' , 'ロミオとシンデレラ']
     songs.each do |song|
-      examine_artistlink('BUMP OF CHICKEN' , url)
       examine_songlink(song , 'BUMP OF CHICKEN' , url)
     end
   end
