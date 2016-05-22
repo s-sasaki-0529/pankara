b86a6c22d24b2acba6103041359cede78b206484
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-05-22 15:21:42 +0900

    refs #544 nginx用にunicornの設定を変更

--- unicorn.rb
+++ unicorn.rb
@@ -6,7 +6,7 @@ worker_processes 1
 working_directory @path
 timeout 300
 preload_app true
-listen "#{@path}/tmp/unicorn.sock"
-pid "#{@path}/tmp/unicorn.pid"
+listen "#{@path}shared/tmp/unicorn.sock" , backlog: 1024
+pid "#{@path}shared/tmp/unicorn.pid"
 stderr_path "#{@path}/logs/stderr.log"
 stdout_path "#{@path}/logs/stdout.log"
