b6f61e9bde12b9e98316d1b4649e192227c1616c
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-05-28 00:33:13 +0900

    refs #409 よく歌う歌手リストを取得するメソッドを実装。マイページで一部置き換え

--- app/controllers/user_route.rb
+++ app/controllers/user_route.rb
@@ -16,7 +16,7 @@ class UserRoute < March
     @histories = @user.histories(:limit => 5 , :page => 1)
     @karaoke_list = @user.get_karaoke 5
     @most_sang_song = @user.get_most_sang_song
-    @most_sang_artist = @user.get_most_sang_artist
+    @most_sang_artist = @user.favorite_artists(:limit => 1)[0]
     @max_score = @user.get_max_score
     erb :user_page
   end
