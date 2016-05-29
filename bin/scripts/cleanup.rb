8f3725fa048b577c84447deb485861b8d58dd1db
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-05-29 11:53:13 +0900

    (no ticket) cleanup.rbのコメントを整理

--- bin/scripts/cleanup.rb
+++ bin/scripts/cleanup.rb
@@ -1,10 +1,14 @@
+#----------------------------------------------------------------------
+# 不要なデータを自動削除するスクリプト
+# 本スクリプトは、cronによって毎朝４時に自動実行されることを想定
+#----------------------------------------------------------------------
 require_relative "../../app/models/util"
 require_relative "../../app/models/song"
 require_relative "../../app/models/artist"
 require_relative "../../app/models/store"
 require_relative "../../app/models/karaoke"
 
-# song
+# 一度も歌われていない楽曲を削除
 songs = DB.new(:SELECT => 'id' , :FROM => 'song').execute_columns
 history = DB.new(:SELECT => 'song' , :FROM => 'history').execute_columns.uniq
 trash_songs = songs - history
@@ -14,7 +18,7 @@ trash_songs.each do |id|
 end
 trash_songs.empty? or DB.new(:DELETE => 1 , :FROM => 'song' , :WHERE_IN => ['id' , trash_songs.length] , :SET => trash_songs).execute
 
-# artist
+# 一曲も楽曲が無いアーティストを削除
 artists = DB.new(:SELECT => 'id' , :FROM => 'artist').execute_columns
 sang_artists = DB.new(:SELECT => 'artist' , :FROM => 'song').execute_columns.uniq
 trash_artists = artists - sang_artists
@@ -24,7 +28,7 @@ trash_artists.each do |id|
 end
 trash_artists.empty? or DB.new(:DELETE => 1 , :FROM => 'artist' , :WHERE_IN => ['id' , trash_artists.length] , :SET => trash_artists).execute
 
-# store
+# 一度も利用されていない店舗を削除
 stores = DB.new(:SELECT => 'id' , :FROM => 'store').execute_columns
 used_stores = DB.new(:SELECT => 'store' , :FROM => 'karaoke').execute_columns.uniq
 trash_stores = stores - used_stores
@@ -34,7 +38,7 @@ trash_stores.each do |id|
 end
 trash_stores.empty? or DB.new(:DELETE => 1 , :FROM => 'store' , :WHERE_IN => ["id" , trash_stores.length], :SET => trash_stores).execute
 
-# karaoke
+# 登録から４８時間経過後も歌唱履歴が登録されていないカラオケを削除
 karaoke_list = DB.new(:SELECT => ['id' , 'name'] , :FROM => 'karaoke' , :WHERE => 'DATE_ADD(created_at, INTERVAL 48 HOUR) < NOW()').execute_all
 karaoke_list.each do |karaoke|
   attend_list = DB.new(:SELECT => 'id' , :FROM => 'attendance' , :WHERE => 'karaoke = ?' , :SET => karaoke['id']).execute_columns
