f91bf1eb5b560af15e2a6504be4df5149e731654
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-05-30 22:40:30 +0900

    (no ticket) 歌唱履歴が存在しない楽曲の詳細ページが開けないバグを修正

--- app/controllers/song_route.rb
+++ app/controllers/song_route.rb
@@ -20,6 +20,7 @@ class SongRoute < March
 
     # 歌唱回数グラフ用データを作成
     sang_histories = @song.tally_sang_count()
+    sang_histories.empty? and return erb :song_detail
     monthly_data = Util.monthly_array(:desc => true)
     monthly_data.each do |m|
       month = m[:month]
