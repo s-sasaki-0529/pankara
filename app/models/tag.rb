f7f183f715e3932fefaf014abfd88a27254573d0
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-06-12 13:38:30 +0900

    refs #577 Tagクラスにて、インスタンス生成時にリストを取得するように修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,22 +6,26 @@ require_relative 'util'
 require_relative 'db'
 class Tag < Base
 
+  attr_reader :list
+
   # classとidを指定してインスタンスを生成
   #--------------------------------------------------------------------
   def initialize(_class , id)
     @class = _class
     @id = id
+    @list = get_list
   end
 
   # list - タグの一覧を取得する
   #--------------------------------------------------------------------
-  def list()
-    DB.new(
+  def get_list()
+    @list = DB.new(
       :SELECT => 'name',
       :FROM => 'tag',
       :WHERE => ['class = ?' , 'object = ?'],
       :SET => [@class , @id]
     ).execute_columns
+    return @list
   end
 
   # add - タグを追加する
