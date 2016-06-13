0c8ec98d9952d843296797b2465f9381f11a391b
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-06-13 22:36:28 +0900

    refs #588 テスト項目作成

--- spec/t/022_song_tag_spec.rb
+++ spec/t/022_song_tag_spec.rb
@@ -7,8 +7,38 @@ init = proc do
 end
 
 # テスト実行
-describe 'タグ機能' do
+describe 'タグ機能' , :js => true do
   before(:all , &init)
   before do
+  end
+
+  describe 'タグ表示' do
+    it 'タグなし' do
+    end
+    it 'タグが１個以上５個未満' do
+    end
+    it 'タグが５個' do
+    end
+  end
+
+  describe 'タグ登録' do
+    it '単一登録' do
+    end
+    it '複数登録' do
+    end
+    it '複数登録(５個以上)' do
+    end
+  end
+
+  describe 'タグ削除' do
+  end
+
+  describe 'タグ検索' do
+    it 'タグ検索へのリンク' do
+    end
+    it '楽曲ページヘのリンク' do
+    end
+    it '歌手ページヘのリンク' do
+    end
   end
 end
