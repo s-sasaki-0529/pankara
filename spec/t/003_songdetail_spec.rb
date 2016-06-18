9c2256ec412b5a98404833615da44f11dbf6d7b1
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-06-18 23:43:10 +0900

    refs #582 テストケース作成

--- spec/t/003_songdetail_spec.rb
+++ spec/t/003_songdetail_spec.rb
@@ -11,19 +11,46 @@ describe '楽曲詳細ページ' , :js => true do
   before(:all,&init)
 
   describe '歌唱回数グラフ' do
+    it '誰も歌っていない楽曲' do
+    end
+    it '一人のユーザのみ歌っている楽曲' do
+    end
+    it '複数のユーザが歌っている楽曲' do
+    end
   end
 
   describe '採点結果グラフ' do
+    it '採点モードの切り替え' do
+    end
+    it '採点記録がない場合' do
+    end
+    it '自分の採点記録のみある場合' do
+    end
+    it '自分と他のユーザの採点記録がある場合' do
+    end
   end
 
   describe 'Youtubeプレイヤー' do
+    it 'URLが登録されている場合' do
+    end
+    it 'URLが登録されていない場合' do
+    end
   end
 
   describe '歌唱履歴' do
-    # 現在は022_song_tag_spec.rbにて個別に実施
+  end
+
+  describe 'タグ' do
+    #現在は022_song_tag_spec.rbにて個別対応
   end
 
   describe 'リンク' do
+    it '歌手名' do
+    end
+    it 'ユーザ名' do
+    end
+    it 'カラオケ' do
+    end
   end
 
 end
