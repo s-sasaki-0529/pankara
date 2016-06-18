dd89262e58012089cbc496d4e37027f3e416fc0e
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-06-18 23:34:21 +0900

    refs #582 テストの雛形作成

--- spec/t/003_songdetail_spec.rb
+++ spec/t/003_songdetail_spec.rb
@@ -3,28 +3,27 @@ include Rbase
 
 # テスト用データベース構築
 init = proc do
-  `zenra init -d 2016_03_14_21_21`
-  `zenra mysql -e 'update history set song = 1 , score_type = 1 where score > 90'`
+  `zenra init -d 2016_06_13_00_24`
 end
 
 # テスト実行
-describe '楽曲詳細ページ' do
+describe '楽曲詳細ページ' , :js => true do
   before(:all,&init)
-  before do
-    login 'sa2knight'
-    visit '/history'
+
+  describe '歌唱回数グラフ' do
   end
-  it 'Youtubeがインラインで表示されているか' do
-    examine_songlink('オンリーロンリーグローリー' , 'BUMP OF CHICKEN')
-    db = DB.new(:SELECT => 'url' , :FROM => 'song' , :WHERE => 'name = ?' , :SET => 'オンリーロンリーグローリー')
-    tube = db.execute_column
-    expect(youtube_links[0].slice(/\w+$/)).to eq tube.slice(/\w+$/)
+
+  describe '採点結果グラフ' do
   end
-  it 'リンクが正常に登録されているか' , :js => true do
-    examine_songlink('オンリーロンリーグローリー' , 'BUMP OF CHICKEN')
-    song_url = page.current_path
-    examine_karaokelink('祝本番環境リリースカラオケ' , song_url)
-    examine_userlink('ないと' , song_url)
+
+  describe 'Youtubeプレイヤー' do
+  end
+
+  describe '歌唱履歴' do
+    # 現在は022_song_tag_spec.rbにて個別に実施
+  end
+
+  describe 'リンク' do
   end
 
 end
