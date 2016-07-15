7752115a8e4dabf09764322e97ccd14930e01edb
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-07-15 17:22:26 +0900

    refs #596 テスト修正準備

--- spec/t/015_search_spec.rb
+++ spec/t/015_search_spec.rb
@@ -7,7 +7,7 @@ init = proc do
 end
 
 # 検索
-def search(word , song_num , artist_num)
+def search(word , song_num , artist_num , tag_num)
   fill_in 'search_word' , with: word
   click_on '検索'
   
@@ -15,10 +15,11 @@ def search(word , song_num , artist_num)
     iscontain('※検索ワードを入力してから検索ボタンを押してください')
     islack("を含む楽曲一覧")
     islack("を含む歌手一覧")
+    islack("を含むタグ一覧")
     return
   end
 
-  if song_num == 0 && artist_num == 0
+  if song_num == 0 && artist_num == 0 && tag_num == 0
     iscontain("\"#{word}\" を含む楽曲、歌手、タグが存在しません")
     return
   end
@@ -46,6 +47,11 @@ def search(word , song_num , artist_num)
   else
     islack("を含む歌手一覧")
   end
+
+  if tag_num > 0
+  else
+    islack("を含むタグ一覧")
+  end
 end
 
 
@@ -59,35 +65,35 @@ describe '楽曲/歌手名検索機能' do
 
   describe '検索結果ページ' do
     it '曲も歌手もヒット'  do
-      search('光' , 1 , 2)
-      search('of' , 3 , 4)
-      search(' ' , 59 , 29)
+      search('光' , 1 , 2 , 0)
+      search('of' , 3 , 4 , 0)
+      search(' ' , 59 , 29 , 0)
       table = table_to_hash('search_song_table')
       expect(table[0]['tostring']).to eq 'Hello, world!,BUMP OF CHICKEN'
       expect(table[5]['tostring']).to eq 'SECRET LOVER,一ノ瀬トキヤ'
     end
 
     it '曲のみヒット' do
-      search('メドレー' , 3 , 0)
-      search('song' , 2 , 0)
+      search('メドレー' , 3 , 0 , 0)
+      search('song' , 2 , 0 , 0)
     end
 
     it '歌手のみヒット' do
-      search('雪音' , 0 , 5)
-      search('未来' , 0 , 2)
+      search('雪音' , 0 , 5 , 0)
+      search('未来' , 0 , 2 , 0)
     end
 
     it 'ヒット無し' do
-      search('SEKAI GA OWARU' , 0 , 0)
-      search('君が代' , 0 , 0)
+      search('SEKAI GA OWARU' , 0 , 0 , 0)
+      search('君が代' , 0 , 0 , 0)
     end
 
     it '入力なし' do
-      search('' , 0 , 0)
+      search('' , 0 , 0 , 0)
     end
 
     it 'リンク' do
-      search('0' , 2 , 1)
+      search('0' , 2 , 1 , 0)
       url = page.current_url
       examine_songlink 'マジLOVE2000%' , 'ST☆RISH' , url
       examine_artistlink '40mP' , url
@@ -95,13 +101,13 @@ describe '楽曲/歌手名検索機能' do
   end
   describe 'リダイレクト' do
     it '楽曲' do
-      search('世界に一つだけの花' , -1 , -1)
+      search('世界に一つだけの花' , -1 , -1 , 0)
       expect(current_path).to eq '/song/62'
       iscontain '世界に一つだけの花'
     end
 
     it 'アーティスト' do
-      search('KAT' , -1 , -1)
+      search('KAT' , -1 , -1 , 0)
       expect(current_path).to eq '/artist/143'
       iscontain 'KAT-TUN'
     end
