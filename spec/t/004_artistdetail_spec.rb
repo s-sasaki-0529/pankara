615333d764ab52f9fae5eceb534c4e89de43cd3c
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-08-16 14:54:38 +0900

    refs #607 テストの雛形作成

--- spec/t/004_artistdetail_spec.rb
+++ spec/t/004_artistdetail_spec.rb
@@ -9,12 +9,34 @@ init = proc do
 end
 
 # テスト実行
-describe '歌手詳細ページ' do
+describe '歌手詳細ページ' , :js => true do
   before(:all,&init)
   before do
     login 'sa2knight'
     visit '/history'
   end
+
+  describe 'Wikipediaが正常に読み込まれるか' do
+    it '記事が存在する' do
+    end
+    it '記事が存在しない' do
+    end
+  end
+
+  describe 'よく歌われる楽曲グラフ' do
+    it 'その他あり' do
+    end
+    it 'その他なし' do
+    end
+  end
+
+  describe '歌唱回数グラフ' do
+    it '一人だけ歌っている' do
+    end
+    it '複数人が歌っている' do
+    end
+  end
+
   it '歌手の楽曲一覧が正常に表示されるか' do
     examine_artistlink 'BUMP OF CHICKEN'
     tables = table_to_hash('artistdetail_table')
@@ -22,6 +44,7 @@ describe '歌手詳細ページ' do
     expect(tables[0]['tostring']).to eq ',オンリーロンリーグローリー,2,0'
     expect(tables[18]['tostring']).to eq '未登録,たったひとつの日々,0,2'
   end
+
   it 'リンクが正常に登録されているか' do
     visit '/history'
     examine_artistlink('BUMP OF CHICKEN')
