f044b2459df4e124b24cb32e164084f87886e284
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-08-18 17:52:23 +0900

    refs #624 カラオケ新規登録時に、時刻の初期値を現在時刻に

--- app/public/script/zenra.js
+++ app/public/script/zenra.js
@@ -806,8 +806,10 @@ var register = (function() {
           beforeClose: beforeClose
         } ,
         func_at_load: function() {
+          s = zenra.formatDate(new Date , 'YYYY/MM/DD hh:mm');
           createWidgetForKaraoke();
-          $('#button1').attr('onclick' , 'register.submitKaraokeRegistrationRequest();').val('次へ');
+          $('#button1').attr('onclick' , 'register.submitKaraokeRegistrationRequest();').val('次へ')
+          $('#datetime').val(s);
         }
       });
     } ,
@@ -1216,3 +1218,19 @@ zenra.removeSongTag = function(user , id , tag) {
     zenra.post(url_to , data , opt);
   });
 };
+
+zenra.formatDate = function (date, format) {
+  if (!format) format = 'YYYY-MM-DD hh:mm:ss.SSS';
+  format = format.replace(/YYYY/g, date.getFullYear());
+  format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+  format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2));
+  format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2));
+  format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+  format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+  if (format.match(/S/g)) {
+    var milliSeconds = ('00' + date.getMilliseconds()).slice(-3);
+    var length = format.match(/S/g).length;
+    for (var i = 0; i < length; i++) format = format.replace(/S/, milliSeconds.substring(i, i + 1));
+  }
+  return format;
+}
