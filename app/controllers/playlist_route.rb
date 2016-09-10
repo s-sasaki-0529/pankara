8ff0a96ad8f93bf72ae731f61a3e9d2c0ef6293a
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-09-11 07:34:25 +0900

    refs #695 再生リストをシャッフル

--- app/controllers/playlist_route.rb
+++ app/controllers/playlist_route.rb
@@ -3,7 +3,9 @@ require_relative '../models/song'
 class PlaylistRoute < March
   get '/' do
     songs = params[:songs].split('_')
-    @songs = Util.array_to_hash(Song.list(:songs => songs , :artist_info => true) , 'song_url')
+    songs_list = Song.list(:songs => songs , :artist_info => true)
+    params[:random] and songs_list.shuffle!
+    @songs = Util.array_to_hash(songs_list , 'song_url')
     @songs_json = Util.to_json(@songs)
     erb :playlist
   end
