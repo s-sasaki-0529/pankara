0ad4e71103a030ef985fb7d99c7d5e251dd9a55e
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-12 21:02:21 +0900

    refs #727 初めてのブランチ分け

---
+++ app/controllers/stat_route.rb
@@ -0,0 +1,11 @@
+require_relative './march'
+
+class StatRoute < March
+
+  # get '/stat' - 仮置き
+  #---------------------------------------------------------------------
+  get '/' do
+    'Hello Stat Page'
+  end
+
+end
