447517ec87080bad6eafb9033383b5d67ab517b1
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-15 18:10:36 +0900

    refs #708 既存の歌唱履歴をスマフォで編集する際のバグを修正

--- app/public/script/zenra.js
+++ app/public/script/zenra.js
@@ -330,7 +330,8 @@ var dialog = function(title , dialog_id , width , height) {
       var div = $('<div></div>');
       div.load(url + " #" + id , function(date , status) {
         if (! zenra.ispc) {
-          $('.ui-dialog').css({'top' : 0 , 'z-index': 9999}); 
+          $('.ui-dialog').css({'top' : 0 , 'z-index': 9999});
+          $(window).scrollTop(0);
         }
         else if (position == 'center') {
           var margin = div.height() / 2;
