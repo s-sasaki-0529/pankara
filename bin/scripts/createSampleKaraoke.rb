88155e8b8c52f688769d888c182023f1c0ef4e11
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-17 21:02:12 +0900

    カラオケ自動生成スクリプトに使い方コメントを付与

--- bin/scripts/createSampleKaraoke.rb
+++ bin/scripts/createSampleKaraoke.rb
@@ -1,3 +1,58 @@
+=begin
+----------------------------------------------------------------------------
+createSampleKaraoke.rb - 実データを用いたkaraokeのサンプルを自動生成する
+
+[実行方法] cdz && bundle exec ruby bin/scripts/createSampleKaraoke
+
+[入力例(キーボードから標準入力)]
+
+カラオケを登録するユーザ: sa2knight
+日付時刻: 2016/10/10 13:00:00
+カラオケ名: テスト
+カラオケ時間: 7
+店舗名(name): カラオケ館
+店舗名(branch): 亀戸店
+機種名(brand): JOYSOUND
+機種名(product): MAX
+カラオケに参加させるユーザの数: 5
+友達のユーザ名: hetare
+友達のユーザ名: unagipai
+友達のユーザ名: tomotin
+友達のユーザ名: mya0ryuta
+友達のユーザ名: worry
+登録する楽曲数(一人あたり)15
+
+[入力例(パイプを用いる場合)]
+
+cdz && cat bin/scripts/createSampleKaraoke_dataSample | bundle exec ruby bin/scripts/createSampleKaraoke.rb 2>&1 > /dev/null
+
+dataのサンプル
+
+sa2knight
+2016/10/10 13:00:00
+テスト
+7
+カラオケ館
+亀戸店
+JOYSOUND
+MAX
+5
+hetare
+unagipai
+tomotin
+mya0ryuta
+worry
+15
+
+[実行結果]
+
+上記の例の場合、６人(sa2knight,hetare,unagipai,tomotin,mya0ryuta,worry)が参加した
+karaokeを生成し、karaokeに対し一人あたり１５曲の歌唱履歴を登録する。
+その際に登録する楽曲は、そのユーザの持ち歌からランダムで選曲されるため、
+より実データに近い結果を得ることができる
+
+=end
+
 require_relative '../../app/models/register'
 require_relative '../../app/models/user'
 
@@ -43,7 +98,7 @@ usernum.times do |i|
   registers[i + 1].attend_karaoke
 end
 
-print '登録する楽曲数(一人あたり)'
+print '登録する楽曲数(一人あたり): '
 STDIN.gets().to_i.times do |i|
   registers.each_with_index do |r , s|
     h = get_song(users[s])
