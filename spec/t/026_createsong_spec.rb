c1a987ff712029cef2b9569ebd94a53d07e30e6a
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-21 23:24:23 +0900

    テストが落ちてたので修正

--- spec/t/026_createsong_spec.rb
+++ spec/t/026_createsong_spec.rb
@@ -6,26 +6,26 @@ init = proc do
   `zenra init -d 2016_08_24_04_00`
 end
 
-# 楽曲を新規登録
-def create_song(song_name , artist_name)
-  js('register.createSong();')
-  fill_in 'song' , with: song_name
-  fill_in 'artist' , with: artist_name
-  find('#button1').click
-  wait_for_ajax
-end
-
-# 指定した楽曲がDBに何曲あるかを検証
-def examine_song_num(song_name , expect_num)
-  result = `zenra mysql -se "select count(id) from song where name = '#{song_name}'"`
-  expect(result.to_i).to eq expect_num
-end
-
 # テスト実行
 describe '楽曲登録機能' , :js => true do
   before(:all , &init)
   before do
     login 'sa2knight'
+  end
+
+  # 楽曲を新規登録
+  def create_song(song_name , artist_name)
+    js('register.createSong();')
+    fill_in 'song' , with: song_name
+    fill_in 'artist' , with: artist_name
+    find('#button1').click
+    wait_for_ajax
+  end
+
+  # 指定した楽曲がDBに何曲あるかを検証
+  def examine_song_num(song_name , expect_num)
+    result = `zenra mysql -se "select count(id) from song where name = '#{song_name}'"`
+    expect(result.to_i).to eq expect_num
   end
 
   it 'ログインしていないと登録できない' do
