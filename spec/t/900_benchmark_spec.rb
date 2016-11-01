666df53f9891342d078561e51dbcc61845c3562d
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-11-01 13:06:08 +0900

    refs #797 ベンチマークスクリプトの実装準備

---
+++ spec/t/900_benchmark_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../rbase'
+include Rbase
+
+describe 'ベンチマーク' do
+  describe 'ユーザ' do
+    it '認証' do
+    end
+    it 'マイページ' do
+    end
+    it '歌唱履歴' do
+    end
+    it '持ち歌一覧' do
+    end
+  end
+  describe 'カラオケ' do
+    it '全てのカラオケ一覧' do
+    end
+    it 'あなたのカラオケ一覧' do
+    end
+    it '詳細画面' do
+    end
+  end
+  describe '楽曲' do
+    it '詳細画面' do
+    end
+    it '楽曲ランキング' do
+    end
+    it '得点ランキング' do
+    end
+    it 'タグ検索' do
+    end
+    it '再生リスト' do
+    end
+  end
+  describe 'アーティスト' do
+    it '詳細画面' do
+    end
+    it 'アーティストランキング' do
+    end
+    it '一覧' do
+    end
+  end
+  describe 'その他' do
+    it '検索' do
+    end
+  end
+end
