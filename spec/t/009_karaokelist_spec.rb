99a3e32735d9e81a5660f97a1c16a9766d8844ca
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-12-12 12:57:10 +0900

    refs #951 テスト実装

--- spec/t/009_karaokelist_spec.rb
+++ spec/t/009_karaokelist_spec.rb
@@ -21,17 +21,17 @@ describe 'カラオケ一覧機能' do
   it '全カラオケ一覧表示' do
     prove(
       'karaoke/list' , 'カラオケ一覧' , 5 , 
-      '2016-02-05,ヒトカラ専用ルーム,1.5,快活CLUB 甚目寺店,DAM(LIVE DAM),')
+      '2016-02-05,ヒトカラ専用ルーム,1.5,快活CLUB 甚目寺店,DAM(LIVE DAM),,13')
   end
   it 'ログインユーザのカラオケ一覧表示' do
     prove(
       'karaoke/user' , 'ないとさんのカラオケ一覧' , 3 , 
-      '2016-01-30,2016年 3/24回目,3.0,歌広場 亀戸店,JOYSOUND(CROSSO),,')
+      '2016-01-30,2016年 3/24回目,3.0,歌広場 亀戸店,JOYSOUND(CROSSO),,40,')
   end
   it '指定したユーザのカラオケ一覧表示' do
     prove(
       'karaoke/user/worry' , 'ウォーリーさんのカラオケ一覧' , 1 , 
-      '2016-01-08,新年初カラオケ,5.0,JOYJOY 甚目寺店,JOYSOUND(MAX),')
+      '2016-01-08,新年初カラオケ,5.0,JOYJOY 甚目寺店,JOYSOUND(MAX),,42')
   end
   describe 'カラオケの件数' do
     it '全カラオケ' do
