c01d5c4996e4cc613744083593f780e575f56262
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-12-26 23:31:28 +0900

    refs #967 歌唱履歴詳細画面から各ページへのリンク

--- app/models/history.rb
+++ app/models/history.rb
@@ -23,6 +23,7 @@ class History < Base
   def set_song_info
     songInfo = Song.new(@params['song']).params
     @params['song_name'] = songInfo['name']
+    @params['artist_id'] = songInfo['artist']
     @params['artist_name'] = songInfo['artist_name']
     @params['url'] = songInfo['url']
   end
