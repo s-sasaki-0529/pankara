428e56c4eac91a36504241bf75e68d704ab53ac9
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-07-02 17:47:39 +0900

    DEV-171 テスト修正

--- spec/t/023_user_aggregate_spec.rb
+++ spec/t/023_user_aggregate_spec.rb
@@ -3,7 +3,7 @@ include Rbase
 
 # テスト用データベース構築
 init = proc do
-  `zenra init -d 2016_08_19_04_00`
+  `zenra init -d 2017_07_02_17_42`
   User.create('test_user' , 'test_user' , 'test_user')
 end
 
@@ -18,10 +18,10 @@ describe '集計情報表示機能' do
     iscontain '集計情報を表示'
     visit '/user/aggregate/sa2knight'
     iscontain ['カラオケ記録' , '歌唱記録' , '機種別集計' , '採点別集計' , 'その他']
-    expect(table_to_hash('karaoke_aggregate_table')[0]['tostring']).to eq '総カラオケ時間,110.5 時間'
-    expect(table_to_hash('sang_aggregate_table')[1]['tostring']).to eq '持ち歌手数,150 組'
-    expect(table_to_hash('product_aggregate_table')[4]['tostring']).to eq 'LIVE DAM,3 回,122 曲'
-    expect(table_to_hash('score_aggregate_table')[3]['tostring']).to eq 'DAM ランキングバトル,63 曲,92.03 点,83.05 点'
+    expect(table_to_hash('karaoke_aggregate_table')[0]['tostring']).to eq '総カラオケ時間,288.0 時間'
+    expect(table_to_hash('sang_aggregate_table')[1]['tostring']).to eq '持ち歌手数,200 組'
+    expect(table_to_hash('product_aggregate_table')[5]['tostring']).to eq 'LIVE DAM,8 回,282 曲'
+    expect(table_to_hash('score_aggregate_table')[3]['tostring']).to eq 'DAM ランキングバトル,76 曲,92.03 点,83.25 点'
   end
 
   it '歌唱履歴のないユーザ' do
@@ -32,7 +32,7 @@ describe '集計情報表示機能' do
     iscontain ['カラオケ記録' , '歌唱記録' , '機種別集計' , '採点別集計' , 'その他']
     expect(table_to_hash('karaoke_aggregate_table')[1]['tostring']).to eq '総出費,0 円'
     expect(table_to_hash('sang_aggregate_table')[1]['tostring']).to eq '持ち歌手数,0 組'
-    expect(table_to_hash('product_aggregate_table')[3]['tostring']).to eq 'JOYSOUND WAVE,0 回,0 曲'
+    expect(table_to_hash('product_aggregate_table')[4]['tostring']).to eq 'JOYSOUND WAVE,0 回,0 曲'
     expect(table_to_hash('score_aggregate_table')[2]['tostring']).to eq 'JOYSOUND その他,0 曲,0.0 点,0.0 点'
   end
 
