dad75612186db500c5ab8f6017b16c60f0d0e476
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-07-12 00:09:30 +0900

    カラオケ詳細画面に採点結果を表示のテスト修正

--- spec/t/001_history_spec.rb
+++ spec/t/001_history_spec.rb
@@ -7,7 +7,7 @@ init = proc do
 end
 
 # 定数
-url = 'http://localhost:8080/history/list/sa2knight'
+url = '/history/list/sa2knight'
 
 # テスト実行
 describe '歌唱履歴ページ' do
@@ -26,12 +26,14 @@ describe '歌唱履歴ページ' do
   end
 
   # 歌唱履歴を評価
-  def examine(row, num, date, song, artist, key, satisfaction = nil)
+  def examine(row, num, date, song, artist, key, score_type, score, satisfaction = nil)
     selector = ".history-table:nth-child(#{row}) td"
     expect(page.all(selector)[0].text).to eq num
     expect(page.all(selector)[1].text).to eq date
     expect(page.all(selector)[3].text).to eq "#{song} / #{artist}"
     expect(page.all(selector)[4].text).to eq "キー: #{key}"
+    expect(page.all(selector)[6].text).to eq score_type
+    expect(page.all(selector)[7].text).to eq score
     expect(page.all(selector)[5].text).to eq satisfaction_stars(satisfaction)
   end
 
@@ -43,20 +45,20 @@ describe '歌唱履歴ページ' do
 
   it '歌唱履歴表示' do
     iscontain '1873 曲中 1 〜 24 曲目を表示中'
-    examine(1, '1873', '2017-05-21', 'Stage of the ground', 'BUMP OF CHICKEN', 0, 8)
-    examine(3, '1871', '2017-05-21', 'メルト', 'supercell', 0, 9)
+    examine(1, '1873', '2017-05-21', 'Stage of the ground', 'BUMP OF CHICKEN', 0, '分析採点', '80.89', 8)
+    examine(3, '1871', '2017-05-21', 'メルト', 'supercell', 0, '分析採点', '85.63', 9)
   end
 
   describe 'ページング' do
     it '2ページ目' do
       visit '?page=2'
-      examine(1, '1849', '2017-05-21', 'YUME日和', '島谷ひとみ', -4, 6)
-      examine(3, '1847', '2017-05-21', '一騎当千', '梅とら', 3, 8)
+      examine(1, '1849', '2017-05-21', 'YUME日和', '島谷ひとみ', -4, '分析採点', '88.23', 6)
+      examine(3, '1847', '2017-05-21', '一騎当千', '梅とら', 3, '分析採点', '84.20', 8)
       iscontain '1873 曲中 25 〜 48 曲目を表示中'
     end
     it '最終ページ' do
       visit '?page=79'
-      examine(1, '1', '2016-01-03', 'オンリーロンリーグローリー', 'BUMP OF CHICKEN', 0)
+      examine(1, '1', '2016-01-03', 'オンリーロンリーグローリー', 'BUMP OF CHICKEN', 0, 'ランキングバトル', '84.00')
       iscontain '1873 曲中 1873 〜 1873 曲目を表示中'
     end
   end
@@ -64,17 +66,17 @@ describe '歌唱履歴ページ' do
   describe '絞り込み' do
     it '曲名検索' do
       visit '?filter_category=song&filter_word=%E6%84%9B&pagenum=24' #愛
-      examine(1, '1797', '2017-05-14', '恋愛裁判', '40mP', 5, 7)
+      examine(1, '1797', '2017-05-14', '恋愛裁判', '40mP', 5, '分析採点', '86.29',7)
       iscontain '11 曲中 1 〜 11 曲目を表示中'
     end
     it '歌手名検索' do
       visit '?filter_category=artist&filter_word=BUMP'
-      examine(3, '1851', '2017-05-21', 'ひとりごと', 'BUMP OF CHICKEN', 0, 5)
+      examine(3, '1851', '2017-05-21', 'ひとりごと', 'BUMP OF CHICKEN', 0, '分析採点', '90.58',5)
       iscontain '186 曲中 1 〜 24 曲目を表示中'
     end
     it 'タグ名検索' do
       visit '?filter_category=tag&filter_word=VOCALOID'
-      examine(2, '1870', '2017-05-21', 'Fire◎Flower', 'absorb', -1, 9)
+      examine(2, '1870', '2017-05-21', 'Fire◎Flower', 'absorb', -1, '分析採点', '86.79',9)
       iscontain '904 曲中 1 〜 24 曲目を表示中'
     end
     it '表示件数変更' do
@@ -91,19 +93,19 @@ describe '歌唱履歴ページ' do
     it '満足度(以上)' do
       visit '?filter_satisfaction=9&filter_satisfaction_class=gt&pagenum=24'
       iscontain '56 曲中 1 〜 24 曲目を表示中'
-      examine(1, '1871', '2017-05-21', 'メルト', 'supercell', 0, 9)
-      examine(5, '1854', '2017-05-21', '君じゃなきゃダメみたい', 'オーイシマサヨシ', 0, 10)
+      examine(1, '1871', '2017-05-21', 'メルト', 'supercell', 0, '分析採点', '85.63',9)
+      examine(5, '1854', '2017-05-21', '君じゃなきゃダメみたい', 'オーイシマサヨシ', 0, '分析採点', '81.10',10)
     end
     it '満足度(未満)' do
       visit '?filter_satisfaction=3&filter_satisfaction_class=lt&pagenum=24'
       iscontain '3 曲中 1 〜 3 曲目を表示中'
-      examine(1, '1600', '2017-03-05', 'アスノヨゾラ哨戒班', 'Orangestar', 4, 2)
-      examine(2, '1560', '2017-02-26', 'サンドリヨン', 'Dios/シグナルP', 0, 2)
+      examine(1, '1600', '2017-03-05', 'アスノヨゾラ哨戒班', 'Orangestar', 4, '分析採点', '71.72',2)
+      examine(2, '1560', '2017-02-26', 'サンドリヨン', 'Dios/シグナルP', 0, '精密採点', '78.00',2)
     end
     it '満足度と曲名混合' do
       visit '?filter_category=artist&filter_word=BUMP+OF+CHICKEN&filter_satisfaction=10&filter_satisfaction_class=eq'
       iscontain '1 曲中 1 〜 1 曲目を表示中'
-      examine(1, '1703', '2017-04-02', 'バトルクライ', 'BUMP OF CHICKEN', 0, 10)
+      examine(1, '1703', '2017-04-02', 'バトルクライ', 'BUMP OF CHICKEN', 0, '精密採点', '82.22',10)
     end
     it '不要なフォームが表示されない' do
       islack '並び順'
@@ -111,9 +113,9 @@ describe '歌唱履歴ページ' do
     end
     it 'リセット' do
       click_on '表示'
-      expect(current_url == url).to eq false
+      expect(current_url.split('?').count).to eq 2
       click_on 'リセット'
-      expect(current_url == url).to eq true
+      expect(current_url.split('?').count).to eq 1
     end
   end
 
